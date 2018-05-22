bdd2707bca8f3e31676767c63bfd730b824ece40
juexiaozhang <juexiao.zhang@u-psud.fr>
2018-05-22 21:05:27 +0200

    exemplaire

--- src/view/gest_exemplaires.php
+++ src/view/gest_exemplaires.php
@@ -1,7 +1,10 @@
 <?php
     session_start();
-    if (!isset($_SESSION["id"]))
+    ob_start();
+    if (!isset($_SESSION["id"])){
         header('Location: vue_connexion.php');
+        die();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="fr-FR" xmlns="http://www.w3.org/1999/xhtml">
@@ -11,7 +14,7 @@
     <title> GestionnaireExemplaire </title>
     <meta charset="UTF-8">
     <style>
-    #header {
+    #tete {
         background-color: #eeeeee;
         color: #00486C;
         text-align: center;
@@ -93,6 +96,70 @@
         text-decoration: underline;
         /*鼠标放上去有下划线*/
     }
+
+    fieldset {
+        margin: auto;
+        padding: 20px;
+        border:1px solid #00486C;
+        width:100%;
+    }
+
+    input[type=text],select,textarea {
+        width: 200px;
+        height: 25px;
+        padding:3px;
+        border:1px solid #AADAEF;
+        border-radius:5px;
+        box-shadow:1px 1px 2px #C0C0C0 inset;
+        color: gray;
+        background-color: #eeeeee;
+    }
+
+    textarea {
+        width: 400px;
+        height: 100px;
+        padding:3px;
+        border:1px solid #AADAEF;
+        border-radius:5px;
+        box-shadow:1px 1px 2px #C0C0C0 inset;
+     /*   color: gray;*/
+        background-color: #eeeeee;
+    }
+
+    input[type=submit] {
+        width:100px;
+        height: 35px;
+        margin-left:5px;
+         
+        background-color: #00486C;
+        padding: 3px;
+        border:1px;
+        border-radius:5px;
+        box-shadow:1px 1px 1px #00486C;
+       
+        font-size: 20px;
+        color: #FFFFFF;
+    }
+    input.chercher {
+         width: 480px;
+        height: 50px;
+        background-color: white;
+        border: none;
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+        font: bold 16px 'lucida sans', 'trebuchet MS', 'Tahoma';
+        font-style: italic;
+    }
+    input.date{
+        width: 40px;
+        height: 25px;
+        padding:3px;
+        border:1px solid #AADAEF;
+        border-radius:5px;
+        box-shadow:1px 1px 2px #C0C0C0 inset;
+        color: gray;
+        background-color: #eeeeee;
+    }
     </style>
 </head>
 
@@ -100,7 +167,8 @@
     <div class="upsudLogo">
         <a href="../view/vue_accueil.php"><img src="../image/upsudLogo.png"/></a>
     </div>
-    <div id="header">
+
+    <div id="tete">
         <br><br><br>
         <a href="../view/vue_accueil.php">
             <h1>Médiathèque de l'Université Paris-Sud</h1> </a>
@@ -127,17 +195,57 @@
         <br/>
     </div>
     <div id="section">
-        <h2>Opération</h2>
-        <input class="operation" type="text" value="id d'exemplaire à supprimer" />
-        <button> Supprimer </button>
-        <br/>
-        <br/>
-        <input class="operation" type="text" value="idArticle d'exemplaire à ajouter" />
-        <button> Ajouter </button>
-        <br/>
-        <br/>
         <h2>Les exemplaires</h2>
+        <?php
+            if(!isset($_SESSION["listEx"])){
+                header('Location:../controler/controleur_exList.php');
+                die();
+            }
+            else
+                echo $_SESSION["listEx"];
+        ?>
+
+        <h2>Opération</h2>
+
+        <div> <fieldset>
+            <legend> <h3>Ajouter un exemplaire </h3></legend>
+            <div style="color: red;font-size: 14px;">
+                <?php if(isset($_SESSION["erreurEx"])){ echo $_SESSION["erreurEx"];}?>
+            </div>
+            <form method="post" action="../controler/controleur_ajouterEx.php">
+    
+                <label for="idArt"> ID Article :  </label>
+                    <input type="text" name="idArt" id="idArt" />
+                <br/>
+                <br/>
+
+                <input type="submit" name ="ajouter" value="Ajouter">
+            </form>
+        </div>
+        <br/>
+        <br/>
+
+        <div> <fieldset>
+            <legend> <h3>Supprimer un exemplaire </h3></legend>
+            <div style="color: red;font-size: 14px;">
+                <?php if(isset($_SESSION["erreurExSup"])){ echo $_SESSION["erreurExSup"];}?>
+            </div>
+            <form method="post" action="../controler/controleur_suppEx.php">
+    
+                <label for="idEx"> ID Exemplaire :  </label>
+                    <input type="text" name="idEx" id="idEx" />
+                <br/>
+                <br/>
+
+                <input type="submit" name ="supprimer" value="Supprimer">
+            </form>
+        </div>
+
     </div>
 </body>
 
 </html>
+<?php 
+unset($_SESSION["listEx"]);
+unset($_SESSION["erreurEx"]);
+unset($_SESSION["erreurSup"]);?>
